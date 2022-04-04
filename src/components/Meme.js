8627a398c5a24c12322cbf97b9db64a04703fe5f
Brad Kay <bradkay12@gmail.com>
2022-04-04 16:24:02 +1200

    added handle text functions and state management

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,9 +2,11 @@ import React from "react";
 import memesData from "../memesData.js";
 
 export default function Meme() {
-  const [memeImage, setMemeImage] = React.useState(
-    "http://i.imgflip.com/1bij.jpg"
-  );
+  const [memeState, setMemeState] = React.useState({
+    url: "http://i.imgflip.com/1bij.jpg",
+    topText: "",
+    bottomText: "",
+  });
   /**
    * Challenge: Save the random meme URL in state
    * - Below the div.form, add an <img /> and set the
@@ -14,19 +16,48 @@ export default function Meme() {
   function getMemeImage() {
     const memesArray = memesData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    setMemeImage(memesArray[randomNumber].url);
+    setMemeState((prevState) => ({
+      ...prevState,
+      url: memesArray[randomNumber].url,
+    }));
   }
+  const handleTopChange = (e) => {
+    setMemeState((prevState) => ({
+      ...prevState,
+      topText: e.target.value,
+    }));
+  };
+  const handleBottomChange = (e) => {
+    setMemeState((prevState) => ({
+      ...prevState,
+      bottomText: e.target.value,
+    }));
+  };
 
   return (
     <main>
       <div className="form">
-        <input type="text" placeholder="Top text" className="form--input" />
-        <input type="text" placeholder="Bottom text" className="form--input" />
+        <input
+          type="text"
+          placeholder="Top text"
+          onChange={handleTopChange}
+          className="form--input"
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          onChange={handleBottomChange}
+          className="form--input"
+        />
         <button className="form--button" onClick={getMemeImage}>
           Get a new meme image 🖼
         </button>
       </div>
-      <img src={memeImage} alt="MemeImg" className="meme--image" />
+      <div className="container">
+        <img src={memeState.url} alt="MemeImg" className="meme--image" />
+        <div className="centered">{memeState.topText}</div>
+        <div className="bottom">{memeState.bottomText}</div>
+      </div>
     </main>
   );
 }
